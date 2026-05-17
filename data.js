dadfeb1a1de390cfbb5895797bf59acaebf90b9f
paulagjantunen-hue <paula.g.jantunen@gmail.com>
2026-05-17 21:37:01 +0000

    m

---
+++ data.js
@@ -0,0 +1,16 @@
+const trees = [
+    {
+        img: "images/tree1.jpg",
+        note: "The wind remembers every branch it has ever touched.",
+        photographer: "A. Mikkola"
+    },
+    {
+        img: "images/tree2.jpg",
+        note: "This forest does not hurry. Neither should you.",
+        photographer: "L. Jensen"
+    },
+    {
+        img: "images/tree3.jpg",
+        note: "Some trees grow quietly just to listen"
+    }
+]
