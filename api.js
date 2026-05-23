30a37a9536f5066669847ff88759cfb48cac81a3
paulagjantunen-hue <paula.g.jantunen@gmail.com>
2026-05-23 09:17:39 +0000

    what

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const UNSPLASH_KEY = "p-_3k80TEl4RIJbrBiaBweKOdo-5l7nSIY3g35qptDo";
 
 async function fetchTreeBatch(page = 1) {
     const url =
-        `https://api.unsplash.com/search/photos` +
+`https://api.unsplash.com/search/photos` +
         `?query=forest%20tree%20nature` +
         `&page=${page}` +
         `&per_page=10` +
@@ -18,8 +18,8 @@ async function fetchTreeBatch(page = 1) {
 
     const data = await res.json();
 
-    rreturndata.results.map(img => ({
+    return data.results.map(img => ({
         img: img.urls.regular,
-        photographer: img.user.name
+        photographer: img.user?.name || "Unknown"
     }));
 }
